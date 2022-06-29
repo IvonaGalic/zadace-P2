727b136313928ba04088b334be97f7e092d7df29
IvonaGalic <108396736+IvonaGalic@users.noreply.github.com>
2022-06-29 13:28:54 +0200

    Update 04_zadaca.py

--- 04_zadaca.py
+++ 04_zadaca.py
@@ -2,6 +2,7 @@
 #vraca podudaranje ukoliko se unese string
 #koji počinje kao prvo slovo vašeg imena, a završava kao prvo slovo prezimena.
 #String mora sadržavati bar jedan broj između 0 i 5 i razmak.
+
 import re
 
 regex = "^k.*[0-5].*\s.*v$"
